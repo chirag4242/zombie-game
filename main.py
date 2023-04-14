6e59216781b086a299860758b63604ca72a1e729
chirag4242 <patilchirag484@gmail.com>
2023-04-14 02:51:22 -0400

    fixed the run function while loop added in main file

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ class Main:
             (WIDTH_OF_WINDOW, HEIGHT_OF_WINDOW))
         self.clock = pygame.time.Clock()
         self.editor = Editor()
-    
-    def run(self):
-        delta_time = self.clock.tick()/1000j
-        self.editor.run(delta_time)
-        pygame.display.update()
 
+    def run(self):
+        while True: 
+            delta_time = self.clock.tick()/1000
+            self.editor.run(delta_time)
+            pygame.display.update()
 
 if __name__ == "__main__":
     main = Main()
