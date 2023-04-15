0b98001f2ed5f58a4588fcb47707bb48e74180ec
Chirag Vasant Patil <70964754+chirag4242@users.noreply.github.com>
2023-04-15 00:04:50 -0400

    Graphic data added for player

--- settings.py
+++ settings.py
@@ -4,6 +4,14 @@ WIDTH_OF_WINDOW = 1280
 HEIGHT_OF_WINDOW = 720
 ANIMATION_SPEED = 8
 
+# Editor data 
+GRAPHIC_DATA = {
+    0: {'style': 'player', 'type': 'object', 'menu': None, 'menu_surf': None, 'preview': None, 'graphics': '/sprites/Hero-Guy-PNG/_Mode-Gun/01-Idle/JK_P_Gun__Idle_000.png'},
+	1: {'style': 'sky',    'type': 'object', 'menu': None, 'menu_surf': None, 'preview': None, 'graphics': None},
+
+
+}
+
 
 # colors 
 LINE_COLOR = 'black'
